24603a51e5e392f413c80eb68b36e357d2f1f5f5
mbu-se-vc <mbu-se-vc@mbu-se-vcdeiMac.local>
2015-05-12 17:33:55 +0800

    upgrade all generators, add help info, and add var_name/model_name for models

--- data/templates/data.js
+++ data/templates/data.js
@@ -1,8 +1,28 @@
+/**
+ * Created by Lark on <%= new Date() %>
+ */
 'use strict';
 
-module.exports = function(mvc){
-  var demo = mvc.dataService.create('demo');
-  demo.getData = function () {
-    return this.daoServices.demo.getData() + '-dataService'
-  };
+/**
+ * You can access these methods by this.dataServices["<%= model_name %>"]
+ * in page services
+ **/
+var <%= var_name %> = module.exports = {};
+
+//TODO: dev your own data service methods
+<%= var_name%>.getData = function * () {
+  return (yield this.daoServices.demo.getData()) + '-dataService';
 };
+
+/**
+ * More usage :
+ *
+    module.exports = function(mvc){
+      var <%= var_name %> = mvc.dataService.create("<%= model_name %>");
+      //TODO: dev your own data service methods
+      <%= var_name %>.getData = function * () {
+        return (yield this.daoServices.demo.getData()) + '-dataService'
+      }
+    };
+
+ **/
